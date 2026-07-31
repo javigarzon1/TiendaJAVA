aca0ec614af3d02ca2218b4e5115b95bb43f07ea
javier Garzon <javi.garzon@hotmail.com>
2026-07-31 09:24:56 +0000

    a

--- src/main/java/com/bohemme/citas/exception/HorarioNoDisponibleException.java
+++
@@ -1,7 +0,0 @@
-package com.bohemme.citas.exception;
-
-public class HorarioNoDisponibleException extends RuntimeException {
-    public HorarioNoDisponibleException(String mensaje) {
-        super(mensaje);
-    }
-}
